2900101ef3524436ee9024ad0b3ba51f492a8b1f
mdshajibazher <mdshajibazher@gmail.com>
2021-04-10 09:08:25 +0600

    project bootstrap , popper, scss added

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row mt-3">
+      <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">
+                Login
+            </div>
+            <div class="card-body">
+                    
+            </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
